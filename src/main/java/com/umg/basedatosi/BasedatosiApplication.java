956d342617d1576ac970ae757aaf044b59f7876e
Lester-Marroquin <lester.marroquin96@gmail.com>
2021-10-14 23:40:20 -0600

    First commit

---
+++ src/main/java/com/umg/basedatosi/BasedatosiApplication.java
@@ -0,0 +1,13 @@
+package com.umg.basedatosi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasedatosiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasedatosiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/basedatosi/BasedatosiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.basedatosi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedatosiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
